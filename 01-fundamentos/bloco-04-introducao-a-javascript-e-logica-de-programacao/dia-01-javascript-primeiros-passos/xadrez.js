09c72f8b1a560f77f756b9a427db06113df95483
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-22 08:10:07 -0300

    todos exercícios rodando

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/xadrez.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/xadrez.js
@@ -1,2 +1,21 @@
-let peca = 'cavalo';
-switch (peca)
+let peca = 'Bispo';
+switch (peca.toLowerCase()) {
+    case 'rei':
+        console.log("movimento vertical, horizontal ou diagonal, uma casa por lance");
+        break;
+    case 'rainha':
+        console.log("movimento vertical, horizontal ou diagonal, uma ou mais casas por lance");
+        break;
+    case 'bispo':
+        console.log("movimento diagonal");
+        break;
+    case 'cavalo':
+        console.log("movimento em L");
+        break;
+    case 'torre':
+        console.log("movimento vertical ou horizontal");
+        break;
+    default:
+        console.log("erro")
+
+    }
