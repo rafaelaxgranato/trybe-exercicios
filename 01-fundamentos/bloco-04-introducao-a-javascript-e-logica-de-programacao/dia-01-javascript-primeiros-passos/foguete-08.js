fa709310d38339459a97e3e65473c2b0dc7328d2
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-21 22:48:27 -0300

    exercicios

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/foguete-08.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/foguete-08.js
@@ -1,13 +1,23 @@
 let salarioBruto = 2000;
+let impostoINSS;
 if (salarioBruto < 1156.94) {
-    let impostoINSS = 0.08;
+    impostoINSS = 0.08;
 }
 if else (salarioBruto >= 1156.94 && salarioBruto < 2594.92) {
-    let impostoINSS = 0.09;
+    impostoINSS = 0.09;
 }
 if else (salarioBruto >= 2594.92 && salarioBruto < 5189.82) {
-    let impostoINSS = .11;
+    impostoINSS = .11;
 }
 if else (salarioBruto >= 5189.82) {
-    let impostoINSS = 570.88;
+    impostoINSS = 570.88;
+}
+
+let salarioBase = salarioBruto - (salarioBruto * impostoINSS)
+let impostoIR;
+if (salarioBase < 1903.98) {
+    impostoIR = 0;
+}
+if else (salarioBase >= 1903.98 && salarioBruto2 < 2826.65) {
+    impostoIR = 
 }
