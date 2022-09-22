494db700023953ba3076b67af55ddaad358da460
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-22 16:15:38 -0300

    exercicios

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exemplo-02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exemplo-02.js
@@ -1,4 +1,11 @@
-var numero = 7;
-for (var contador = 1; contador <= 9; contador = contado++;) {
+let numero = 9;
+for (let contador = 1; contador <= 9; contador = contador += 1) {
+    console.log("")
     console.log(numero * contador);
+}
+
+let listaDeNomes = ["Joana", "Maria", "Lucas"];
+for (let indice = 0; indice < listaDeNomes.length; indice += 1) {
+    let mensagem = "Boas vindas, " + listaDeNomes[indice] + "!";
+    console.log(mensagem);
 }
