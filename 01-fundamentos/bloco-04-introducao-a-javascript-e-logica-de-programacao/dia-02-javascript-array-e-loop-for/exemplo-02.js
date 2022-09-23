727d7258ca7f48cc9495b1310bbaf656213c1df6
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-22 22:08:22 -0300

    exercicios

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exemplo-02.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exemplo-02.js
@@ -9,3 +9,11 @@ for (let indice = 0; indice < listaDeNomes.length; indice += 1) {
     let mensagem = "Boas vindas, " + listaDeNomes[indice] + "!";
     console.log(mensagem);
 }
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+console.log(cars[0]); // Saab
+console.log(cars[1]); // Volvo
+console.log(cars[2]); // BMW
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
