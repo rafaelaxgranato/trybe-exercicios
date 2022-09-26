5ef1d6b15659a766d029783385f5f1c7d9920ee2
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-26 09:35:30 -0300

    exercicio conta palavras

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-03.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-03.js
@@ -1,4 +1,13 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = array[0];
+let menor = array[0];
 for (let index = 0; index < array.length; index += 1) {
-    
+    if (menor.length < array[index].length) {
+        menor = array[index];
+    }
+    if (maior.length > array[index].length) {
+        maior = array[index];
+    }
 }
+console.log(maior);
+console.log(menor);
