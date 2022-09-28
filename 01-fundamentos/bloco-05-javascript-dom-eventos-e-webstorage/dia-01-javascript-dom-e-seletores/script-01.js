2c07b00b552453e1d8dd132247f8351bac7c453e
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-28 20:24:57 -0300

    dia 5.1

---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-webstorage/dia-01-javascript-dom-e-seletores/script-01.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Le Fabuleux Destin d'Amélie Poulain"
+document.getElementById("second-paragraph").innerText = "Filmes franceses são demais!!!"
+document.getElementById("subtitle").innerText = "O Fabuloso Destino de Amélie Poulain"
---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-webstorage/dia-01-javascript-dom-e-seletores/script-03.js
@@ -0,0 +1,1 @@
+document.getElementById("head-container").style.color = "green";
