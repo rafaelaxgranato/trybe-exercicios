7504078a9cd9d759ec23e30b216ff925c231b31f
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-29 08:28:19 -0300

    exercicios 5.1

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-webstorage/dia-01-javascript-dom-e-seletores/script-03.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-webstorage/dia-01-javascript-dom-e-seletores/script-03.js
@@ -1,1 +1,22 @@
-document.getElementById("head-container").style.color = "green";
+const header = document.getElementById("header-container");
+header.style.backgroundColor = "green";
+
+const emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasks.style.backgroundColor = "pink";
+
+const emergencyTasksHeaders = document.querySelectorAll(".emergency-tasks h3");
+for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+    emergencyTasksHeaders[index].style.backgroundColor = "purple";
+}
+
+const noEmergencyTasks = document.getElementsByClassName("no-emergency-tasks")[0];
+noEmergencyTasks.style.backgroundColor = "yellow";
+
+const noEmergencyTasksHeaders = document.querySelectorAll(".no-emergency-tasks h3");
+for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+    noEmergencyTasksHeaders[index].style.backgroundColor = "black";
+}
+
+
+const footer = document.getElementById("footer-container");
+footer.style.backgroundColor = "gray";
