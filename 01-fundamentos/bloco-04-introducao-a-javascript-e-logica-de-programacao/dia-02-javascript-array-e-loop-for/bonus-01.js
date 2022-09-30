fc702fc61681868eca917f477e30d6abf071a0d2
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-09-30 08:55:56 -0300

    bonus

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/bonus-01.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/bonus-01.js
@@ -22,7 +22,7 @@ console.log(numbers);
 numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let array = [];
 for (let index = 0; index < numbers.length; index += 1) {
-    if (index +1 < numbers.length) {
+    if (index + 1 < numbers.length) {
         array.push(numbers[index] * numbers[index +1]);
     }
     else {
