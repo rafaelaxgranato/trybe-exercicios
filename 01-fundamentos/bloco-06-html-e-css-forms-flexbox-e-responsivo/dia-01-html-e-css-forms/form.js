cbcf3ba325525e855ffedb4a64032cb664e8363c
rafaelaxgranato <rafaelaxgranato@gmail.com>
2022-10-18 10:58:33 -0300

    exercicios bloco 6

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/form.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/form.js
@@ -1,6 +1,6 @@
-// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault().
-// Crie um botão que limpe as informações contidas nos campos.
-// (Bônus) A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+Interrompa o comportamento padrão do botão submit utilizando o método preventDefault().
+Crie um botão que limpe as informações contidas nos campos.
+(Bônus) A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
 
 window.onload = function () {
     const clearButton = document.getElementById('clear');
